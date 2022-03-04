7f1bea0fdbd6ead544e6c8421d760fec7240322d
MahmoudYazid <60329627+MahmoudYazid@users.noreply.github.com>
2022-03-04 02:09:44 +0200

    Add files via upload

--- new.py
+++ new.py
@@ -6,19 +6,19 @@ data = pd.read_csv("C:\\Users\\ahmed\\Desktop\\pyprojects\\diabetes.csv")
 
 y = np.array(data['Outcome'])
 
-x = pd.DataFrame(data[['Pregnancies', 'Glucose', 'BloodPressure',
+x = pd.DataFrame(data[[ 'Glucose', 'BloodPressure',
                        'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']]).to_numpy()
 
 
 x=np.array(x)
 
-collection_arr = [[1, 93, 70, 31, 0, 30.4, 0.315, 23, 0],[6, 190, 92, 0, 0, 35.5, 0.278, 66, 1],[2, 88, 58, 26, 16, 28.4, 0.766, 22, 0]
-    ,[9, 170, 74, 31, 0, 44, 0.403, 43, 1]
-    ,[9, 89, 62, 0, 0, 22.5, 0.142, 33, 0]
-    ,[10, 101, 76, 48, 180, 32.9, 0.171, 63, 0]
-    ,[2, 122, 70, 27, 0, 36.8, 0.34, 27, 0]
-    ,[5, 121, 72, 23, 112, 26.2, 0.245, 30, 0]
-,[1	,126	,60	,0	,0	,30.1	,0.349	,47	,1]]
+collection_arr = [[ 93, 70, 31, 0, 30.4, 0.315, 23],[ 190, 92, 0, 0, 35.5, 0.278, 66],[ 88, 58, 26, 16, 28.4, 0.766, 22]
+    ,[ 170, 74, 31, 0, 44, 0.403, 43]
+    ,[ 89, 62, 0, 0, 22.5, 0.142, 33]
+    ,[ 101, 76, 48, 180, 32.9, 0.171, 63]
+    ,[ 122, 70, 27, 0, 36.8, 0.34, 27]
+    ,[ 121, 72, 23, 112, 26.2, 0.245, 30]
+,[126	,60	,0	,0	,30.1	,0.349	,47	]]
 
 
 
@@ -29,11 +29,12 @@ no_of_result=0
 n=0
 for arr in collection_arr:
     pred_arr = arr
-    newsize_to_predict=np.resize(pred_arr, [1,8])
+    newsize_to_predict=np.resize(pred_arr, [1,7])
 
 
     newprd=model.predict(newsize_to_predict)
-    print(newprd, " ", "expected", " ", ":", " ", result_test[n])
+    print(newprd, " ", "expected", " ", ":", " ",
+          result_test[n], " ", result_test[n]==newprd[0])
     print()
     n+=1
 
